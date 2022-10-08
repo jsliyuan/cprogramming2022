31b4340926d5c19052e1cfb2138f503c0fcbe072
jsliyuan <jsliyuan@gmail.com>
2022-10-08 19:23:07 +0800

    Update prime3.c

--- week5/prime3.c
+++ week5/prime3.c
@@ -6,7 +6,6 @@ int main() {
   scanf("%d", &n);
 
   int is_prime[n + 1];
-
   for (int i = 2; i <= n; i ++) {
     is_prime[i] = 1;
   }
@@ -14,11 +13,12 @@ int main() {
   for (int i = 2; i <= n; i ++) {
     if (is_prime[i]) {
       printf("%d\n", i);
-    }
-    int j = i + i;
-    while (j <= n) {
-      is_prime[j] = 0;
-      j += i;
+
+      int j = i + i;
+      while (j <= n) {
+        is_prime[j] = 0;
+        j += i;
+      }
     }
   }
 
